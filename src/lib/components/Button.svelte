69c131d8dc75f605a13617e692a293d892726230
Blockbeard2552 <joey.stephens524@gmail.com>
2025-01-08 15:17:12 -0500

    JDS-2 Hero Section & JDS-4 Button Component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface ButtonProps {
+		children: Snippet;
+		onclick: (e: MouseEvent) => void | (() => void);
+		className?: string;
+	}
+	let { children, className, ...props }: ButtonProps = $props();
+</script>
+
+<button class={`btn ${className}`}>
+	{@render children()}
+</button>
+
+<style>
+	.btn {
+		font-family: 'Inter Tight', sans-serif;
+		font-weight: 500;
+		background-color: black;
+		color: white;
+		font-size: 1.2rem;
+		padding: 1rem 2rem;
+		border: none;
+		border-radius: 10px;
+		cursor: pointer;
+		transition: all 0.3s ease-in-out;
+	}
+</style>
---
+++ src/lib/components/Sections/HeroSection.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { Button } from '$components';
+
+	function onclick() {
+		goto('/#contact-form');
+	}
+</script>
+
+<section class="hero-section default-margin">
+	<h3>Hello! I'm Joey</h3>
+	<div class="underscore mb-m"></div>
+	<h1>I am a Svelte developer specializing in</h1>
+	<h1 class="dark-grey mb-m">business management systems and interactive user experiences.</h1>
+	<Button {onclick}>Let's Talk!</Button>
+</section>
+
+<style>
+	.hero-section {
+		padding-top: 60px;
+	}
+</style>
