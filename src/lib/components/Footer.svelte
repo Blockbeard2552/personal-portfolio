2620bf260607810a87c72e15e75cab27e5afd77e
Blockbeard2552 <joey.stephens524@gmail.com>
2025-01-11 17:47:41 -0500

    JDS-25 Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,33 @@
+<footer class="footer">
+	<div class="footer-container">
+		<h3 class="semi-bold mb-xs">Joseph D. Stephens</h3>
+		<div class="mb-l">
+			<a href="https://github.com/Blockbeard2552" target="_blank" class="footer-link">GitHub</a>
+			<a href="https://www.facebook.com/joey.stephens.7/" target="_blank" class="footer-link"
+				>Facebook</a
+			>
+			<a
+				href="https://www.linkedin.com/in/joseph-d-stephens-9128b1299/"
+				target="_blank"
+				class="footer-link">LinkedIn</a
+			>
+			<a href="https://www.youtube.com/@josephdstephens6504" target="_blank" class="footer-link"
+				>YouTube</a
+			>
+		</div>
+		<p>Copyright © 2025 Joseph D. Stephens</p>
+	</div>
+</footer>
+
+<style>
+	.footer {
+		background-color: black;
+		color: white;
+		padding: 40px 20px;
+		text-align: center;
+	}
+
+	.footer-link {
+		margin-right: 0 10px;
+	}
+</style>
